4082c0ece41c79c6cb83dcee1cb0c092742ef806
Your Name <you@example.com>
2019-03-31 20:16:49 +0800

    配置babel-loader

--- build/webpack.config.js
+++ build/webpack.config.js
@@ -1,10 +1,12 @@
 const path = require('path')
+const HTMLPlugin = require('html-webpack-plugin')
 
 function resolvePath(filePath) {
   return path.join(__dirname, filePath);
 }
 
 module.exports = {
+  mode: 'development',
   entry: {
     app: resolvePath('../client/app.js')
   },
@@ -12,5 +14,23 @@ module.exports = {
     filename: '[name].[hash].js',
     path: resolvePath('../dist'),
     publicPath: ''
-  }
+  },
+  module: {
+    rules: [
+      {
+        test: /.jsx$/,
+        loader: 'babel-loader'
+      },
+      {
+        test: /js$/,
+        loader: 'babel-loader',
+        exclude: [
+          resolvePath('../node_modules')
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new HTMLPlugin()
+  ]
 }
